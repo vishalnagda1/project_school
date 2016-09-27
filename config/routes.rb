786007065dc816b4cc185d418c19501799320c3f
Vishal Nagda <vishalnagda1@gmail.com>
2016-09-27 17:32:55 +0530

    Set default index page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
-  get 'welcome/index'
+  root :to =>'welcome#index'
 
   get 'classrooms/index_by_school_id', to: 'classrooms#index_by_school_id'
   get 'subjects/index_by_school_id', to: 'subjects#index_by_school_id'
