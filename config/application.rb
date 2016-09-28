3c89f7cf04b2d116effbbeb1f9c0e86c22864af9
Vishal Nagda <vishalnagda1@gmail.com>
2016-09-28 15:19:46 +0530

    updated application.rb and precompile it with 'rake assets:precompile'

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module ProjectSchool
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.paths << Rails.root.join("app","assets","fonts")
+    Rails.application.config.assets.precompile += %w( index.js common/jquery.min.js school/school_dashboard.js)
   end
 end
