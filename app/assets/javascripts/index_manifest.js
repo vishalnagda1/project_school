f509d0a6c81e8a98ee55bf1fed08d6d60c8353dd
Vishal Nagda <vishalnagda1@gmail.com>
2016-09-29 10:24:13 +0530

    added bootstrap js file

--- app/assets/javascripts/index_manifest.js
+++ app/assets/javascripts/index_manifest.js
@@ -1,4 +1,5 @@
-//= require index.js
 //= require common/jquery.min.js
+//= require common/bootstrap.min.js
+//= require index.js
 //= require school/school_dashboard.js
 //= require common/jquery.dataTables.min.js
